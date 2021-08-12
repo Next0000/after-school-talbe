5dfadf552450ba9a9637a40fe86f1105af01a485
Next0000 <78160512+Next0000@users.noreply.github.com>
2021-08-13 04:46:55 +0900

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -5,21 +5,4 @@ window.onload = function() {
 
   const logo = document.getElementById('title');
   logo.classList.add('move');
-
-//ふわっと
-var scrollAnimationElm = document.querySelectorAll('.sa');
-var scrollAnimationFunc = function() {
-  for(var i = 0; i < scrollAnimationElm.length; i++) {
-    var triggerMargin = 200;
-    if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
-      scrollAnimationElm[i].classList.add('show');
-    }
-  }
-}
-window.addEventListener('load', scrollAnimationFunc);
-window.addEventListener('scroll', scrollAnimationFunc);
-
-
-
-
 }
