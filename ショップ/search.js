f7a53cd62ff5e30cc196f66ddcef9a26320ef238
Next0000 <78160512+Next0000@users.noreply.github.com>
2021-10-06 16:46:58 +0900

    Add files via upload

--- ショップ/search.js
+++ ショップ/search.js
@@ -1,3 +1,9 @@
+//ロード画面の表示
+
+window.onload = function() {
+  const spinner = document.getElementById('loading');
+  spinner.classList.add('loaded');
+}
 
 function submit() {
   const results = [];
