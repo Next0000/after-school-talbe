a1342dc8208807b18179aefc82d42c2223b0fd05
Next0000 <ellipse1997.rect1997@gmail.com>
2024-12-25 22:43:47 +0900

    20241225

--- scripts/include.js
+++ scripts/include.js
@@ -1,5 +1,5 @@
 const includeHeader = new XMLHttpRequest();
-includeHeader.open("GET", "/after-school-talbe/scripts/include/header.html", true);
+includeHeader.open("GET", "/after-school-talbe/include/header.html", true);
 includeHeader.onreadystatechange = function () {
   if (includeHeader.readyState === 4 && includeHeader.status === 200) {
     const headerHTML = includeHeader.responseText;
